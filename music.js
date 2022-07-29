ffed0df43ef1cfc8e4a32d4f9188c31eb6a05c5e
Deepak-1230 <76043632+Deepak-1230@users.noreply.github.com>
2022-07-29 23:48:58 +0530

    New

---
+++ music.js
@@ -0,0 +1,107 @@
+console.log("Welcome to SWARAM");
+
+let songIndex = 0;
+let audioElement = new Audio('1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
+
+let songs = [
+    {songName: "Meri jaan", filePath: "1.mp3", coverPath: "img/m1.jpg"},
+    {songName: "Perfect", filePath: "2.mp3", coverPath: "img/m2.jpg"},
+    {songName: "Jab Saiyaan", filePath: "3.mp3", coverPath: "img/m3.jpg"},
+    {songName: "Senorita", filePath: "4.mp3", coverPath: "img/m4.jpg"},
+    {songName: "Lut gaye", filePath: "5.mp3", coverPath: "img/m5.jpg"},
+    {songName: "Mehabooba", filePath: "6.mp3", coverPath: "img/m6.jpg"},
+    {songName: "Falak tak", filePath: "7.mp3", coverPath: "img/m7.jpg"},
+    {songName: "Pasoori", filePath: "8.mp3", coverPath: "img/m8.jpg"},
+    {songName: "Bhool Bhulaiyaan", filePath: "9.mp3", coverPath: "img/m9.jpg"},
+    {songName: "Barbaadiyaan", filePath: "10.mp3", coverPath: "img/m10.jpg"},
+]
+
+songItems.forEach((element, i)=>{ 
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
+})
+ 
+
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+    }
+})
+
+audioElement.addEventListener('timeupdate', ()=>{ 
+    
+    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
+    myProgressBar.value = progress;
+})
+
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
+
+const makeAllPlays = ()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
+    })
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{ 
+        makeAllPlays();
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src = `${songIndex+1}.mp3`;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    })
+})
+
+document.getElementById('next').addEventListener('click', ()=>{
+    if(songIndex>=9){
+        songIndex = 0
+    }
+    else{
+        songIndex += 1;
+    }
+    audioElement.src = `${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+
+})
+
+document.getElementById('previous').addEventListener('click', ()=>{
+    if(songIndex<=0){
+        songIndex = 0
+    }
+    else{
+        songIndex -= 1;
+    }
+    audioElement.src = `${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
